e8ec0164c217d85fafe20e332bb82212c3eb959d
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2025-07-22 14:45:40 +0900

    ✨ parse args

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,30 @@
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// A flag to indicate preparation
+    #[arg(short, long)]
+    prepare: bool,
+
+    /// A flag to indicate hashing
+    #[arg(short = 'H', long)]
+    hash: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    if args.prepare {
+        println!("'--prepare' or '-p' option is provided.");
+    } else {
+        println!("'--prepare' or '-p' option is NOT provided.");
+    }
+
+    if args.hash {
+        println!("'--hash' or '-H' option is provided.");
+    } else {
+        println!("'--hash' or '-H' option is NOT provided.");
+    }
 }
